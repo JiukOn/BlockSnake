04d80fd0fdab7543b00650d46d29541491ef3667
JiukOn <jp252529@gmail.com>
2022-01-18 22:46:26 -0300

    mkSnake

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-function criarBG(){
+let snake = [];
+
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+function mkBG(){
     context.fillStyle = 'rgb(134, 54, 255)';
     context.fillRect(0,0,16*box,16*box);
 }
 
-criarBG();
+function mkSnake(){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = 'rgb(128, 76, 237)';
+        context.strokeStyle = 'rgb(34, 24, 56)';
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+        context.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+mkBG();
+mkSnake();

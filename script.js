7e0679e515686a2428e665b5f7e432019b41b467
JiukOn <jp252529@gmail.com>
2022-01-18 22:25:23 -0300

    Secure Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+function criarBG(){
+    context.fillStyle = 'rgb(134, 54, 255)';
+    context.fillRect(0,0,16*box,16*box);
+}
+
+criarBG();
